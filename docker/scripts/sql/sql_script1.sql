6a8255cc77767e98976c6133832f4ef45c932ec8
kawnur <kawnur@yandex.ru>
2025-05-10 14:44:47 +0300

    Added json field via logstash

--- docker/scripts/sql/sql_script1.sql
+++ docker/scripts/sql/sql_script1.sql
@@ -24,6 +24,7 @@ create table test3 (
 
 create table test4 (
     timepoint timestamp without time zone,
+    service_ip varchar(30),
     field1 varchar(30),
     field2 varchar(30),
     field3 varchar(30),
